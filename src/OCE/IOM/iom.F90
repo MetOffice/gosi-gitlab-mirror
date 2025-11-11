a94d6b07ac88298f9f0febe0de327437635d45e0
Daley Calvert <14-dcalvert@users.noreply.forge.nemo-ocean.eu>
2025-11-11 10:28:02 +0000

    Correction to ln_cfmeta fix

--- src/OCE/IOM/iom.F90
+++ src/OCE/IOM/iom.F90
@@ -2246,9 +2246,9 @@ CONTAINS
             CALL xios_set_domain_attr( cldom, ni_glo = ni_glo, nj_glo = nj_glo, ibegin = ibegin, jbegin = jbegin, ni = ni, nj = nj,   &
                &    lonvalue_1D = lonvalue, latvalue_1D = latvalue, bounds_lon_1D = bounds_lon, bounds_lat_1D = bounds_lat,           &
 #if defined key_xios3
-               &    mask_1D = mask, nvertex = nvertex, area_2d = area, TYPE = 'curvilinear', data_dim = data_dim )
+               &    mask_1D = mask_1D, nvertex = nvertex, area_2d = area, TYPE = 'curvilinear', data_dim = data_dim )
 #else
-               &    mask_1D = mask, nvertex = nvertex, area = area, TYPE = 'curvilinear', data_dim = data_dim )
+               &    mask_1D = mask_1D, nvertex = nvertex, area = area, TYPE = 'curvilinear', data_dim = data_dim )
 #endif
 
             IF( lldogrd ) THEN                                            ! add new grid definitions
