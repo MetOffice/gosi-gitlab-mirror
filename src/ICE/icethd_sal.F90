3f3b1ffc5c4901349332e8a40833f108859cc295
Clement Rousset <clement.rousset@locean.ipsl.fr>
2026-05-06 10:25:15 +0000

    Resolve "tuning gravity drainage in sea-ice"

--- src/ICE/icethd_sal.F90
+++ src/ICE/icethd_sal.F90
@@ -167,7 +167,7 @@ CONTAINS
       !! Thomas, M., Vancoppenolle, M., France, J. L., Sturges, W. T., Bakker, D. C. E., Kaiser, J., & von Glasow, R. (2020).
       !!             Tracer measurements in growing sea ice support convective gravity drainage parameterizations.
       !!             Journal of Geophysical Research: Oceans, 125, e2019JC015791. https://doi.org/10. 1029/2019JC015791
-      !! Ortega, E., Vancoppenolle, M., Rousset, C., & Lemaire, E. (2025).
+      !! Ortega, E., Vancoppenolle, M., Rousset, C., & Lemaire, E. (2026).
       !!             New representation of sea ice salt dynamics within NEMO-SI3: Evaluation and impacts.
       !!---------------------------------------------------------------------
       INTEGER  ::   ji, jk, jk2            ! dummy loop indices 
