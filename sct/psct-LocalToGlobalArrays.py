a6be268720078bd6f5d3a3e6d1fa0ee7020c93b6
Simon Mueller <11-smueller@users.noreply.forge.nemo-ocean.eu>
2026-06-29 08:34:15 +0000

    Resolve "Transitional maintenance of the PSyclone-based source-code processing option" (8, 'main')

--- sct/psct-LocalToGlobalArrays.py
+++ sct/psct-LocalToGlobalArrays.py
@@ -4,6 +4,7 @@
 # ======================================================================
 #  History : 5.0  !  2024  (S. Mueller)
 #            5.0  !  2025-01  (S. Mueller) Update for compatibility with the latest PSyclone release version
+#            5.1. !  2026-06  (S. Mueller) Adjustment for PSyclone-3.2.2 compatibility
 # ----------------------------------------------------------------------
 #
 # PSyclone transformation script for the promotion of some local arrays to
@@ -13,13 +14,13 @@
 # which is applied to most procedures.
 #
 # ----------------------------------------------------------------------
-# NEMO 5.0 , NEMO Consortium (2025)
+# NEMO 5.1.a, NEMO Consortium (2026)
 # Software governed by the CeCILL license (see ./LICENSE)
 # ----------------------------------------------------------------------
 
 from psyclone.psyir.transformations import HoistLocalArraysTrans
 
-# For compatibility with PSyclone release version 3.0.0
+# For PSyclone-3 compatibility
 from psct_utils import P3APICompat
 
 # ----------------------------------------------------------------------
@@ -42,6 +43,9 @@ def trans(psy):
     if not len([ m for m in MODULES_REJECT if psy.name.lower()=='psy_'+m+'_psy' ]):
         for invoke in psy.invokes.invoke_list:
             if not invoke.name.lower() in INVOKES_REJECT:
-                HoistLocalArraysTrans().apply(invoke.schedule)
+                # Avoid the processing of procedures with the
+                # 'ELEMENTAL' attribute
+                if not invoke.schedule.symbol.is_elemental:
+                    HoistLocalArraysTrans().apply(invoke.schedule)
 
     return
